7b46571a4d5429a9f56e13c0c816daf1ced3c593
Andreas Karis <ak.karis@gmail.com>
2021-11-29 12:04:49 +0100

    e2e

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -4,18 +4,20 @@
 echo "Moving to /tmp"
 pushd /tmp
 
-# echo "Building e2e tests - this can take a while"
-# git clone https://github.com/kubernetes/kubernetes
-# cd kubernetes
-# git checkout -t origin/release-1.21
-# make WHAT="test/e2e/e2e.test"
-# \cp ./_output/bin/e2e.test /usr/local/bin/e2e.test
+if ! [ -f /usr/local/bin/e2e.test ]; then
+echo "Building e2e tests - this can take a while"
+git clone https://github.com/kubernetes/kubernetes
+cd kubernetes
+git checkout -t origin/release-1.21
+make WHAT="test/e2e/e2e.test"
+\cp ./_output/bin/e2e.test /usr/local/bin/e2e.test
 
-echo "Downloading e2e tests"
+# echo "Downloading e2e tests"
 # from https://github.com/ovn-org/ovn-kubernetes/blob/master/test/scripts/install-kind.sh
-curl -L "https://github.com/trozet/ovnFiles/blob/master/kubernetes-test-linux-v1.21.0-alpha.0.341%2B46d481b4556e33.tar.gz?raw=true" -o kubernetes-test-linux-amd64.tar.gz
-tar xvzf kubernetes-test-linux-amd64.tar.gz
-mv ./e2e.test /usr/local/bin/e2e.test
+#curl -L "https://github.com/trozet/ovnFiles/blob/master/kubernetes-test-linux-v1.21.0-alpha.0.341%2B46d481b4556e33.tar.gz?raw=true" -o kubernetes-test-linux-amd64.tar.gz
+# tar xvzf kubernetes-test-linux-amd64.tar.gz
+# mv ./e2e.test /usr/local/bin/e2e.test
+fi
 
 echo "Running network conformance tests"
 export KUBECONFIG="${HOME}/.kube/config"
